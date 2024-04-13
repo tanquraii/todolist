70a1539272a6602f193946356712fce8766c1240
tanqurai <higashigatajosuke16@gmail.com>
2024-04-13 16:35:09 +0600

    second commit

--- src/Advice.js
+++ src/Advice.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import './index.css'
 import { useState } from 'react';
-import axios from 'axios'
+import axios from 'axios';
 const Advice = () => {
     const [advice,setAdvice] = useState("");
     const makeAdvice = async ()=>{
